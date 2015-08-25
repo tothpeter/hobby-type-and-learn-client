a50e5833cde2b457f046e3ae1d129303eade0813
Peter Toth <tothpeter08@gmail.com>
2015-08-25 20:12:19 +0200

    User more proper variable names

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
-import ENV from '../config/environment';
+import config from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-  host: ENV.apiHost
+  host: config.apiHost
 });
