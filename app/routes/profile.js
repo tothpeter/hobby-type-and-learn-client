52214592a76eeb2fd352be5d678e9180414af971
Peter Toth <tothpeter08@gmail.com>
2015-08-30 17:35:51 +0200

    Add create new label functionality

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin);
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  actions: {
+    createLabel: function() {
+      var controller = this.controllerFor('profile.labels');
+      var label = this.store.createRecord('label', controller.getProperties('title'));
+      label.save();
+    }
+  }
+});
