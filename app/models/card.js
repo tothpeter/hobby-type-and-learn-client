df12faf6b96428d59c9d3545b84062d0d1667858
Peter Toth <tothpeter08@gmail.com>
2015-08-31 21:07:15 +0200

    Fix card model

--- app/models/card.js
+++ app/models/card.js
@@ -1,9 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  sidA: DS.attr(),
+  sideA: DS.attr(),
   sideB: DS.attr(),
-  rating: DS.attr('numeric'),
+  rating: DS.attr('number'),
 
   user: DS.belongsTo('user'),
   labels: DS.hasMany('label', { async: true }),
