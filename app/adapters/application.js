07701384b69bfdeb4f2e328ccb7a41de38d24f71
Peter Toth <tothpeter08@gmail.com>
2015-09-01 22:12:07 +0200

    Set coalesceFindRequests to true so we need send only one request with the list of ids

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 import config from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-  host: config.apiHost
+  host: config.apiHost,
+  coalesceFindRequests: true
 });
