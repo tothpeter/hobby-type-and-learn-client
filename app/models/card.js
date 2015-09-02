24266f0539102c03fec5a4a7e2456d31052411a2
Peter Toth <tothpeter08@gmail.com>
2015-09-02 21:24:19 +0200

    Change attribute name in card model

--- app/models/card.js
+++ app/models/card.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   sideA: DS.attr(),
   sideB: DS.attr(),
-  rating: DS.attr('number'),
+  proficiencyLevel: DS.attr('number'),
 
   user: DS.belongsTo('user'),
   labels: DS.hasMany('label', { async: true }),
