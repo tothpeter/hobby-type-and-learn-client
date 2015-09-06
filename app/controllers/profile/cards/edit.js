c2567712133ed9f12b7c3c6d083571fc67a27cad
Peter Toth <tothpeter08@gmail.com>
2015-09-06 19:51:29 +0200

    Fix: make sure cart update goes through controller validation

--- app/controllers/profile/cards/edit.js
+++ app/controllers/profile/cards/edit.js
@@ -5,7 +5,7 @@ export default Ember.Controller.extend(PostValidations, {
   showErrors: true,
 
   actions: {
-    createCard: function() {
+    updateCard: function() {
       return this.get('isValid');
     },
 
