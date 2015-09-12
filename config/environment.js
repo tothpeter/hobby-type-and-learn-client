0eddba7219e1194e2ae562f4fafe26aba1f7491b
Peter Toth <tothpeter08@gmail.com>
2015-09-12 17:22:43 +0200

    Don't use subdomain of api in development

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
 
-    ENV.apiHost = 'http://api.type-and-learn-api.dev';
+    ENV.apiHost = 'http://type-and-learn-api.dev';
     ENV.contentSecurityPolicy = contentSecurityPolicy;
     ENV.contentSecurityPolicy['script-src'] = "'self' 'unsafe-eval'";
 
