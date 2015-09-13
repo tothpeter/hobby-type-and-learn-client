a46a4b29cf2322860a99558378ed963238f0149b
Peter Toth <tothpeter08@gmail.com>
2015-09-13 22:55:41 +0200

    Add basic acceptance test for creating new label

--- tests/acceptance/labels-test.js
+++ tests/acceptance/labels-test.js
@@ -36,3 +36,32 @@ test('visiting /profile', function(assert) {
   });
 
 });
+
+
+test('create new label', function(assert) {
+  server = new Pretender(function() {
+    this.get('current_user', function() {
+      var expectedResponseData = {"data":{"id":"2","type":"users","attributes":{"email":"tothpeter08@gmail.com"},"relationships":{"labels":{"data":[{"type":"labels","id":"2"},{"type":"labels","id":"1"},{"type":"labels","id":"37"}]}}},"included":[{"id":"2","type":"labels","attributes":{"title":"label 2","position":0,"user_id":2},"relationships":{"user":{"data":{"type":"users","id":"2"}}}},{"id":"1","type":"labels","attributes":{"title":"asd","position":2,"user_id":2},"relationships":{"user":{"data":{"type":"users","id":"2"}}}},{"id":"37","type":"labels","attributes":{"title":"asd12","position":null,"user_id":2},"relationships":{"user":{"data":{"type":"users","id":"2"}}}}]};
+
+      return [200, {"Content-Type": "application/json"}, JSON.stringify(expectedResponseData)];
+    });
+
+    this.post('labels', function() {
+      var expectedResponseData = {"data":{"id":"38","type":"labels","attributes":{"title":"Test Label Title","position":null,"user_id":2},"relationships":{"user":{"data":{"type":"users","id":"2"}}}}};
+
+      return [201, {"Content-Type": "application/json"}, JSON.stringify(expectedResponseData)];
+    });
+  });
+
+  authenticateSession();
+
+  visit('/profile');
+  click('.list-group > button');
+
+  fillIn('.modal-dialog input', 'Test Label Title');
+  click('button:contains("OK")');
+
+  andThen(function() {
+    assert.equal(find('.list-group-item:contains("Test Label Title")').length, 1, "Displays newly created label in the list");
+  });
+});
