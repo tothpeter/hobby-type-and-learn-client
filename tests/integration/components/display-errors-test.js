4112a59fc34a5a4611027413752b4f902bacb8b0
Peter Toth <tothpeter08@gmail.com>
2015-09-13 17:26:21 +0200

    Fix the test of display-error component

--- tests/integration/components/display-errors-test.js
+++ tests/integration/components/display-errors-test.js
@@ -6,22 +6,18 @@ moduleForComponent('display-errors', 'Integration | Component | display errors',
   integration: true
 });
 
-test('it renders', function(assert) {
-  assert.expect(2);
-
+test('it renders correctly', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  
+  this.set('showErrors', true);
+  this.set('errors', ['error message1', 'error message2']);
 
-  this.render(hbs`{{display-errors}}`);
+  this.render(hbs`{{display-errors errors=errors showErrors=showErrors}}`);
 
-  assert.equal(this.$().text(), '');
+  assert.equal(this.$().text().trim(), 'error message1');
+  
+  this.set('showErrors', false);
 
-  // Template block usage:
-  this.render(hbs`
-    {{#display-errors}}
-      template block text
-    {{/display-errors}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), '');
 });
