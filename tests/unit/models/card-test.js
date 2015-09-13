841df6459bd6526f826a90fb2b0e8762c53693a6
Peter Toth <tothpeter08@gmail.com>
2015-09-13 16:34:11 +0200

    Fix cards model test

--- tests/unit/models/card-test.js
+++ tests/unit/models/card-test.js
@@ -1,6 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('cards', 'Unit | Model | cards', {
+moduleForModel('card', 'Unit | Model | card', {
   // Specify the other units that are required for this test.
   needs: []
 });
