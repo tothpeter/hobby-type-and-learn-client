0b0f781f700b07209200faf5549d79cacc4316c2
Peter Toth <tothpeter08@gmail.com>
2015-09-13 17:42:43 +0200

    Fix basic model tests

--- tests/unit/models/label-test.js
+++ tests/unit/models/label-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('label', 'Unit | Model | label', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:user', 'model:card']
 });
 
 test('it exists', function(assert) {
