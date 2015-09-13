b32ffca98db5766a6b6f3837698b6bb569da327c
Peter Toth <tothpeter08@gmail.com>
2015-09-13 12:20:05 +0200

    Set auth token after successful login

--- app/instance-initializers/custom-session.js
+++ app/instance-initializers/custom-session.js
@@ -13,6 +13,12 @@ function initialize(application) {
 
       var user = store.peekRecord('user', preloadedUser.data.id);
       this.set('content.currentUser', user);
+
+      var date = new Date();
+      date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+      var expires = '; expires=' + date.toUTCString();
+
+      document.cookie = 'auth_token_for_web=' + this.get('content.secure.token') + expires + '; path=/; domain=' + location.host + ';';
     }
   });
 }
