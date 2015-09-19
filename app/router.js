be4af0e99e09077ce0a2cdec9a6608cdf1793a47
Peter Toth <tothpeter08@gmail.com>
2015-09-19 19:54:34 +0200

    Add basic about and index page add enhance navbar

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+
   this.route('profile', function() {
     this.route('labels', {path: 'labels/:id'}, function() {
       this.route('cards');
