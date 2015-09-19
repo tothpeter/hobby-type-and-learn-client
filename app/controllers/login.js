7a7adc75725b5ec89e6a2b2ea502b8a441d0383e
Peter Toth <tothpeter08@gmail.com>
2015-09-20 01:42:16 +0200

    Using disabled and spinner on login form

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isLoading: false,
+
   actions: {
     authenticate: function() {
       var credentials = this.getProperties('identification', 'password');
@@ -9,13 +11,16 @@ export default Ember.Controller.extend({
         return false;
       }
 
+      this.set('isLoading', true);
+
       var _this = this;
 
       this.get('session').authenticate('simple-auth-authenticator:devise', credentials).then(function() {
         _this.session.setCurrentUser();
         _this.transitionToRoute('profile');
+        _this.set('isLoading', false);
       }, function(response) {
-        // authentication failed
+        _this.set('isLoading', false);
         alert(response.errors);
       });
     }
