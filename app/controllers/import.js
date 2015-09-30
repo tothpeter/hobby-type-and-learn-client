e61ae3c0a7fa9d7afbf9255370338080c1fb0d55
Peter Toth <tothpeter08@gmail.com>
2015-09-30 22:42:22 +0200

    Activate tabulator insert in textarea on import

--- app/controllers/import.js
+++ app/controllers/import.js
@@ -18,6 +18,31 @@ export default Ember.Controller.extend(PostValidations, {
   separatorType: 'tab',
   separatorOther: '',
 
+  setupTextarea: function() {
+    Ember.run.schedule('afterRender', function() {
+      Ember.$('.ember-text-area').on('keydown', function(event) {
+        if (event.keyCode === 9) {
+
+          // get caret position/selection
+          var val = this.value,
+              start = this.selectionStart,
+              end = this.selectionEnd;
+
+          // set textarea value to: text before caret + tab + text after caret
+          this.value = val.substring(0, start) + '\t' + val.substring(end);
+
+          // put caret at right position again
+          this.selectionStart = this.selectionEnd = start + 1;
+
+          // prevent the focus lose
+          event.preventDefault();
+          return false;
+        }
+      });
+    });
+  }.on('init'),
+
+
   separator: Ember.computed('separatorType', 'separatorOther', function() {
     const separators = {
       'tab': '\t',
